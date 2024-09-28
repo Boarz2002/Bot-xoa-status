4060f05152fbed443acc4b7e18a57ab9fc7e929d
Boarz2002 <dopin2002@gmail.com>
2024-09-28 17:24:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Đang chill cùng Boarz","Chơi MINIGAME đi","Xem trộm Sapaw tắm];
+const statusMessages = ["Đang chill cùng Boarz","Chơi MINIGAME đi","Xem trộm Sapaw tắm"];
 
 
 let currentIndex = 0;
